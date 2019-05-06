5a9f273a7fdf40b71b72b4ee48b173a9779e65ad
Robert <panescurobert@yahoo.com>
2019-05-06 22:52:43 +0300

    first push

---
+++ app/src/main/java/com/example/robert/speedometer/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.robert.speedometer;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.security.Provider;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView startTime;
+
+    final int MY_LOCATION_PERMISION_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startTime = findViewById(R.id.startTime);
+
+        // Register class as listener
+        LocationManager locationObject = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Check permission
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_LOCATION_PERMISION_CODE);
+        } else if (locationObject != null) {
+
+            LocationListener locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+                    if (location == null) {
+                        startTime.setText("-.- m/s");
+                    } else {
+                        float nCurrentSpeed = location.getSpeed();
+                        startTime.setText(nCurrentSpeed + " m/s");
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+                }
+            };
+
+
+            Criteria criteria = new Criteria();
+//            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            String provider = locationObject.getBestProvider(criteria, true);
+            locationObject.requestLocationUpdates(provider, 0, 0, locationListener);
+//            locationObject.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+//            locationObject.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+        }
+    }
+}
